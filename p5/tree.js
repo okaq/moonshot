2a53ebccb23278aba9398acdaaafca15de21c658
aq <aq@okaq.com>
2020-11-03 11:04:26 -0500

    Rect tree

---
+++ p5/tree.js
@@ -0,0 +1,25 @@
+// okaq tree
+// created by <aq@okaq.com>
+// 2020-11-03
+
+function setup() {
+  createCanvas(3840,2160);
+  noLoop();
+}
+
+function draw() {
+  background(128,128,128);
+}
+
+class rect {
+  constructor(w0, h0) {
+    this.w0 = w0;
+    this h0 = h0;
+  }
+  
+  print() {
+    console.log(w0,h0);
+  }
+}
+
+
