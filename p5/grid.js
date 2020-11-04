a055b3186f55cc566813dd0760e7d13a0e5db5a3
aq <aq@okaq.com>
2020-11-04 08:31:24 -0500

    Grid for turing pattern

---
+++ p5/grid.js
@@ -0,0 +1,16 @@
+// okaq turing pattern
+// created by <aq@okaq.com>
+// 2020-11-04
+function setup() {
+  createCanvas(3840,2160);
+  noLoop();
+}
+
+function draw() {
+  background(128,128,128);
+  // random alpha
+  let r0 = random(0,1);
+  console.log("rand: " + r0);
+}
+
+
