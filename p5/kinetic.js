e2ad77d0661cbe89caae0ca4ff4f8e6a19dfff04
aq <aq@okaq.com>
2020-11-25 10:40:24 -0500

    Kinetic lines

---
+++ p5/kinetic.js
@@ -0,0 +1,22 @@
+// kinetic curves
+// created by aq <aq@okaq.com>
+// 2020-11-25
+function setup() {
+    let w0 = 1024;
+    let h0 = 1024;
+    createCanvas(w0, h0);
+    noLoop();
+  }
+  
+  function draw() {
+    background(128);
+    grid();
+  }
+  
+  function grid() {
+    strokeWeight(2);
+    stroke(0);
+    // x-y loop
+  }
+  
+  
