327f5f976b43ab071be15d783d4f012760784f3c
aq <aq@okaq.com>
2020-11-26 12:37:13 -0500

    Add global vars

--- p5/kinetic.js
+++ p5/kinetic.js
@@ -1,9 +1,22 @@
 // kinetic curves
 // created by aq <aq@okaq.com>
 // 2020-11-25
+
+// globals
+const w0 = 1024;
+const h0 = 1024;
+
+const w1 = 32;
+const h1 = 32;
+
+const nx = w0 / w1;
+const ny = h0 / h1;
+const nt = nx * ny;
+console.log(nx,ny,nt);
+
 function setup() {
-    let w0 = 1024;
-    let h0 = 1024;
+    // let w0 = 1024;
+    // let h0 = 1024;
     createCanvas(w0, h0);
     noLoop();
   }
