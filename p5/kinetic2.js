f7da7c8c23a5f8901e8e3404c844cf996ce2201a
aq <aq@okaq.com>
2020-12-01 10:59:07 -0500

    Kinetic part two

---
+++ p5/kinetic2.js
@@ -0,0 +1,30 @@
+// kinetic curves v.2.0
+// created by aq <aq@okaq.com>
+// 2020-12-01
+
+// globals
+const w0 = 1024;
+const h0 = 1024;
+
+let line0 = [];
+let lineN = 256;
+
+let tick = 0;
+let max = 1024;
+let id = null;
+
+function setup() {
+	createCanvas(w0,h0);
+	noLoop();
+}
+
+function draw() {
+	background(8);
+	// loop, gen
+}
+
+// todo: setup p5js async load from cdn
+// defer script run from static dir via golang localhost
+
+
+
