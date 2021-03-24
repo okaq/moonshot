aa35b838ec58b6d0d013851d0ffcf36baf72b422
aq <aq@okaq.com>
2021-03-24 09:13:14 -0400

    DOM init

--- txt/web.go
+++ txt/web.go
@@ -11,6 +11,8 @@ import (
 
 const (
 	INDEX = "web.html"
+	INDEX2 = "web2.html"
+	DATA = "txt_0.json"
 )
 
 func motd() {
@@ -23,9 +25,21 @@ func WebHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w,r,INDEX)
 }
 
+func DataHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
+	http.ServeFile(w,r,DATA)
+}
+
+func Web2Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
+	http.ServeFile(w,r,INDEX2)
+}
+
 func main() {
 	motd()
 	http.HandleFunc("/", WebHandler)
+	http.HandleFunc("/a", DataHandler)
+	http.HandleFunc("/b", Web2Handler)
 	// http.Handle("/img/", http.StripPrefix("/img", http.FileServer(http.Dir("img/"))))
 	http.ListenAndServe(":8080", nil)
 }
