d2246661d39bde7ff5fe6862702aa2c5ebe60f92
aq <aq@okaq.com>
2021-03-29 19:17:36 -0400

    Achive request handler

--- ac/web.go
+++ ac/web.go
@@ -5,12 +5,14 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"time"
 )
 
 const (
 	INDEX = "web.html"
+	ARCHIVE = "https://calder.org/archive/all/works/"
 )
 
 func motd() {
@@ -28,10 +30,24 @@ func SaveHandler(w http.ResponseWriter, r *http.Request) {
 	// save image to disk
 }
 
+func ArchiveHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
+	res, err := http.Get(ARCHIVE)
+	if err != nil {
+		fmt.Println(err)
+	}
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	w.Write(body)
+}
+
 func main() {
 	motd()
 	http.HandleFunc("/", WebHandler)
 	http.HandleFunc("/a", SaveHandler)
+	http.HandleFunc("/b", ArchiveHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
