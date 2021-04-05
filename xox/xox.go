7a754f6e23e97b9e94d07f6ab280bce22434b1c8
aq <aq@okaq.com>
2021-04-05 08:28:48 -0400

    New file xox2.html

--- xox/xox.go
+++ xox/xox.go
@@ -11,6 +11,7 @@ import (
 
 const (
 	INDEX = "xox.html"
+	INDEX2 = "xox2.html"
 )
 
 func motd() {
@@ -22,9 +23,15 @@ func XonixHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w,r,INDEX)
 }
 
+func Xonix2Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
+	http.ServeFile(w,r,INDEX2)
+}
+
 func main() {
 	motd()
 	http.HandleFunc("/", XonixHandler)
+	http.HandleFunc("/a", Xonix2Handler)
 	http.ListenAndServe(":8080", nil)
 }
 
