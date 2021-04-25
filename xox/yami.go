8f4b6de2de75a53161f272201f84167ce7b28bf5
aq <aq@okaq.com>
2021-04-25 14:56:40 -0400

    Yami web serve

--- xox/yami.go
+++ xox/yami.go
@@ -4,4 +4,33 @@
 // aq@okaq.com
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+const (
+	INDEX = "yami.html"
+)
+
+func motd() {
+	fmt.Println("okaq web serve on localhost:8080")
+	fmt.Println(time.Now().String())
+}
+
+func YamiHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
+	http.ServeFile(w,r,INDEX)
+}
+
+func main() {
+	motd()
+	http.HandleFunc("/", YamiHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+// to be hosted on okaq.gtihub.io
+// no need to write log files for now
+
 
