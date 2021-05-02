19661566e58e9098ce08205fccccd425f5a8fb97
aq <aq@okaq.com>
2021-05-02 16:25:03 -0400

    Web serve notes

---
+++ yami/ami.go
@@ -0,0 +1,35 @@
+// CFP deploy
+// 2021-05-02
+// aq@okaq.com
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+const (
+	INDEX = "zami.html"
+)
+
+func motd() {
+	fmt.Println("okaq web serve on localhost:8080")
+	fmt.Println(time.Now().String())
+}
+
+func YamiHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
+	http.ServeFile(w,r,INDEX)
+}
+
+func main() {
+	motd()
+	http.HandleFunc("/", YamiHandler)
+	htp.ListenAndServe(":8080", nil)
+}
+
+// set up map[string]string cache
+// json api to read, write, update
+// message queue using channel
+
